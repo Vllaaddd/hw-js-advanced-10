646afbda7f1ce58968414c7fc2c665e8e9585a53
Vlad <vladikhoncharuk@gmail.com>
2023-03-28 20:05:56 +0300

    commit

--- src/script.js
+++ src/script.js
@@ -1,23 +1,67 @@
-import pokemonCardTpl from "./templates/pokemon-card";
+import { createClient } from 'pexels';
+import imagesCardTPL from "./templates/image-card";
 
-const pokemonCard = document.querySelector('.js-card-container');
-const input = document.querySelector('.form-control');
-const btn = document.querySelector('.btn');
+const searchForm = document.querySelector('.form-control');
+const loadMoreBtn = document.querySelector('[data-action="load-more"]');
+const showLessBtn = document.querySelector('[data-action="show-less"]');
+const btnFetch = document.querySelector('.btn-fetch');
+const imagesContainer = document.querySelector('.js-card-container');
 
-btn.addEventListener('click', (e) => {
+let pageSize = 10;
+btnFetch.disabled = true
+loadMoreBtn.style.display = 'none';
+showLessBtn.style.display = 'none';
+
+searchForm.addEventListener('input', e => {
+     
+    if(searchForm.value.length == 0){
+        btnFetch.disabled = true 
+    }else{
+        btnFetch.disabled = false
+    }
+
+})
+
+btnFetch.addEventListener('click', e => {
     e.preventDefault();
-    const number = input.value;
 
-    fetch(`https://pokeapi.co/api/v2/pokemon/${number}`)
-    .then(response => response.json())
-    .then(pokemon => {
-        const markup = pokemonCardTpl(pokemon)
+    const query = searchForm.value;
 
-        pokemonCard.innerHTML = markup;
-    })
-    .catch((err) => {
-        alert('Не вдалось знайти покемона')
-    })
+    fetchSmth(query)
 
-    input.value = '';
-});
+    loadMoreBtn.addEventListener('click', onLoadMore);
+
+    function onLoadMore(){
+        pageSize += 10;
+    
+        fetchSmth(query)
+    }
+
+    showLessBtn.addEventListener('click', onShowLess);
+
+    function onShowLess(){
+        if(pageSize > 10){
+            pageSize -= 10;
+        }else if (pageSize <= 10){
+        }
+    
+        fetchSmth(query)
+    }
+
+    searchForm.value = '';
+    showLessBtn.style.display = 'block';
+    loadMoreBtn.style.display = 'block';
+})
+
+function fetchSmth(query){
+
+    const client = createClient('xf6QFcVzShVimELdDWtaeMhs3jLvQJSR7dfEl5EC9mqoj5BdPUq548rQ');
+
+    client.photos.search({ query, per_page: pageSize })
+        .then(photos => {
+            const markup = imagesCardTPL(photos);
+
+            imagesContainer.innerHTML = markup
+        });
+
+}
